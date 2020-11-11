fc3b26fa94feab25d7471dd9e89dbd44af58d6c7
Lucas Soto <lucaslsoto95@gmail.com>
2020-11-11 17:11:16 -0300

    [ADD] web related modules: move from calyx-servicios/odoo-calyx

---
+++ custom_menus/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Menus",
+
+    'summary': """
+        Oculta menus innecesarios para implementaciones pequeñas""",
+
+    'description': """
+        
+    """,
+    'author': "Calyx",
+    'website': "http://www.calyxservicios.com.ar",    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/menus.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ sale_purchase_attachment/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Purchase Attachment",
+    'summary': """ """,
+    'description': """ """,
+    'author': "Calyx",
+    'website': "http://www.calyxservicios.com.ar",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['crm','purchase','sale','account','base'],
+    'data': [],
+}
